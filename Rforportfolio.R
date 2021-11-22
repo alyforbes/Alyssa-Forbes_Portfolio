e9d1f667084790722da30e049bf60e11f49b1e29
Alyssa Forbes <94805330+alyforbes@users.noreply.github.com>
2021-11-21 20:25:04 -0400

    Add files via upload

---
+++ Rforportfolio.R
@@ -0,0 +1,10 @@
+#setwd
+
+setwd('/Users/AlyssaForbes/Forbes_PROSIT/Social_Analytics')
+
+#Separate into the columns
+
+colnames(mydata2)[1] = "column"
+
+df = data.frame(do.call("rbind", strsplit(as.character(mydata2$column), "|", fixed = TRUE)))
+
